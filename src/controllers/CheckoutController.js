9290607029bfda4b1eec7e6f4ae055f10217ee62
Marcos Vidal <markos_ari@hotmail.com>
2024-05-23 19:43:27 -0300

    ADD backs url success

--- src/controllers/CheckoutController.js
+++ src/controllers/CheckoutController.js
@@ -6,7 +6,13 @@ export const createCheckoutPreference = async (req, res) => {
         const client = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN });
         const preference = new Preference(client);
         console.log(body)
-        const response = await preference.create({body})
+        const response = await preference.create({
+            body: {
+                ...body,
+                back_urls: {
+                    success: `${process.env.FRONT_URL}/checkout/success`
+                } 
+            }})
         res.json({
             ok: true,
             preferenceId: response.id
